6ca73ad0cb2f4479dfde8952e0d8a27c9b26ac3e
Stefano Sergio <stefano@fathomlondon.com>
2017-06-21 19:36:38 +0100

    wiring up redux for creation

--- src/components/create-employee.component.js
+++ src/components/create-employee.component.js
@@ -2,18 +2,23 @@ import React, { Component } from 'react';
 import { View, Text, Button } from 'react-native';
 import { connect } from 'react-redux';
 import { Card, CardSection, Input } from './common';
+import {InputChangedAction} from '../actions/employee.actions';
 
 class CreateEmployee extends Component {
     state = {}
+
+    inputUpdated(prop, value) {
+        this.props.dispatch(new InputChangedAction({prop, value}));
+    }
 
     render() {
         return (
             <Card>
                 <CardSection>
-                    <Input label="Name" placeholder="Jane" />
+                    <Input label="Name" placeholder="Jane" onChangeText={text => this.inputUpdated('name', text)} />
                 </CardSection>
                 <CardSection>
-                    <Input label="Phone" placeholder="0123" />
+                    <Input label="Phone" placeholder="0123" onChangeText={text => this.inputUpdated('phone', text)} />
                 </CardSection>
                 <CardSection>
                     <Input label="email" placeholder="Jane" />
@@ -25,4 +30,10 @@ class CreateEmployee extends Component {
         );
     }
 }
-export default CreateEmployee;
+
+const mapStateToProps = (state) => {
+    const {name, phone, shift} = state.employeeCreate;
+    return {name, phone, shift};
+};
+
+export default connect(mapStateToProps)(CreateEmployee);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import AuthReducer from './auth.reducer';
+import EmployeeCreateReducer from './employee.reducer';
 
 export default combineReducers({
-    auth: AuthReducer
+    auth: AuthReducer,
+    employeeCreate: EmployeeCreateReducer
 });
