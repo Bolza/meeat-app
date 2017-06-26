93d488bab235835c30574b79efaf19c845c9c2ed
Stefano Sergio <stefano@fathomlondon.com>
2017-06-26 20:05:09 +0100

    show list

--- src/components/employee-list.component.js
+++ src/components/employee-list.component.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { View, Text } from 'react-native';
+import { View, Text, ListView, ListItem } from 'react-native';
 import {FetchEmployeesAction} from '../actions/employee.actions';
 
 class EmployeeListComponent extends Component {
@@ -8,20 +8,34 @@ class EmployeeListComponent extends Component {
 
     componentWillMount() {
         this.props.dispatch(new FetchEmployeesAction());
+        this.dataSource = new ListView.DataSource({
+            rowHasChanged: (r1, r2) => r1 !== r2
+        });
+        this.dataSource = this.dataSource.cloneWithRows(this.props.employeeList);
+    }
+
+    componentWillReceiveProps(next) {
+        this.dataSource = this.dataSource.cloneWithRows(next.employeeList);
+    }
+
+    renderRow(item) {
+        return <Text>{item.name}</Text>;
     }
 
     render() {
         return (
-            <View>
-                <Text>Element1</Text>
-            </View>
+            <ListView
+                enableEmptySections
+                dataSource={this.dataSource}
+                renderRow={this.renderRow}
+            />
         );
     }
 }
 
 const mapStateToProps = (state) => {
     const {employeeList} = state.employees;
-    console.log(employeeList)
+    console.log(employeeList);
     return {employeeList};
 };
 
