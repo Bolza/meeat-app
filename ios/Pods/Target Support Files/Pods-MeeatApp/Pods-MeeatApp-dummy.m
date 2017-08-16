7ad920438991bbecc19bf40a82ce14143292cf9b
Stefano Sergio <stefano@fathomlondon.com>
2017-08-16 14:37:16 +0100

    cocoapod added

---
+++ ios/Pods/Target Support Files/Pods-MeeatApp/Pods-MeeatApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MeeatApp : NSObject
+@end
+@implementation PodsDummy_Pods_MeeatApp
+@end
---
+++ ios/Pods/Target Support Files/Pods-MeeatAppTests/Pods-MeeatAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MeeatAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_MeeatAppTests
+@end
